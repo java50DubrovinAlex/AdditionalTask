49fd433ea883d95d77f7f457b46a23a28ee2db4b
Alexander <caxagsh@gmail.com>
2023-06-18 21:16:03 +0300

    add task last ver

--- src/additionalTask/AdditionalTasks.java
+++ src/additionalTask/AdditionalTasks.java
@@ -1,87 +1,75 @@
 package additionalTask;
 
 public class AdditionalTasks {
-	
-	
+
 	// #3
 	public static boolean isAnagram(String sourceWord, String targetWord) {
-		int [] helper = new int [Short.MAX_VALUE];
+		int[] helper = new int[Short.MAX_VALUE];
 		int index = 0;
 		char[] sourceWordArr = sourceWord.toLowerCase().toCharArray();
 		char[] targetWordArr = targetWord.toLowerCase().toCharArray();
-		for(int i = 0; i < sourceWordArr.length;i++) {
-			if(sourceWordArr[i] < 97 || sourceWordArr[i] > 122 && targetWordArr[i] < 97 || targetWordArr[i] > 122) {
+		for (int i = 0; i < sourceWordArr.length; i++) {
+			if (sourceWordArr[i] < 97 || sourceWordArr[i] > 122 && targetWordArr[i] < 97 || targetWordArr[i] > 122) {
 				return false;
 			}
 			helper[sourceWordArr[i]] += 1;
 			helper[targetWordArr[i]] -= 1;
 		}
-		while(index < helper.length && helper[index] == '\u0000') {
+		while (index < helper.length && helper[index] == '\u0000') {
 			index++;
 		}
-		 return index == helper.length ? true : false;
+		return index == helper.length ? true : false;
 	}
-	
-	
-	
-	
-	
-	
-	
-	//#4
+	// #4
 	public static boolean hasValidBrackets(String text) {
-		char[] helper = text.toCharArray();
-		int [] brecketArray = new int [helper.length];
-		int index = 0;
-		int checkIndex = 0;
-		boolean res = true;
-		while(index >= 0 && checkIndex >= 0 && index < helper.length) {
-			if(index == 0 && checkCloseBrecket(helper, index) < 0) {
-				res = false;
-			}else 
-				if(checkOpenBrecket(helper, index) > 0) {
-					brecketArray[checkIndex] = checkOpenBrecket(helper, index);
+		char[] array = text.toCharArray();
+		char[] openBrecket = new char[array.length];
+		char[] closeBrecket = new char[array.length];
+		char[] brecketCheackArr = new char[array.length];
+		int indexOpen = 0;
+		int indexClose = 0;
+		int countOfBreckets = 0;
+		int j = 0;
+		for (int i = 0; i < brecketCheackArr.length; i++) {
+			if (checkBrecket(array, i) != 0) {
+				brecketCheackArr[j] = array[i];
+				j++;
+				countOfBreckets += 1;
+			}
+		}
+		for (int i = 0; i < countOfBreckets; i++) {
+			if (countOfBreckets % 2 != 0) {
+				return false;
+			}
+			if (checkBrecket(brecketCheackArr, i) < 0 && indexOpen == 0) {
+				return false;
+			} else if (checkBrecket(brecketCheackArr, i) > 0) {
+				openBrecket[indexOpen] = brecketCheackArr[i];
+				indexOpen++;
+			}
+			if (checkBrecket(brecketCheackArr, i) < 0 && indexOpen > 0) {
+				closeBrecket[indexClose] = brecketCheackArr[i];
+				if (checkBrecket(openBrecket, indexOpen - 1) + checkBrecket(closeBrecket, indexClose) == 0) {
+					indexOpen--;
+				} else {
+					return false;
 				}
-			if(checkIndex > 0 && brecketArray[checkIndex - 1] + checkCloseBrecket(helper, index) == 0) {
-				brecketArray[checkIndex - 1] += checkCloseBrecket(helper, index);
-				checkIndex -= 3;
-			}else 
-			if(checkIndex == 0 && brecketArray[checkIndex ] + checkCloseBrecket(helper, index) == 0) {
-				brecketArray[0] += checkCloseBrecket(helper, index);
-			}
-			
-			checkIndex++;
-			index++;
-			
-		}
-		
-		return brecketArray[0] == 0 && res? true : false;
-		
-		
-	}
-
-	private static int checkCloseBrecket(char[] helper, int index) {
-		int res = 0;
-		char[] breket = { ')', ']', '}' };
-		for (int i = 0; i < breket.length; i++) {
-			if (breket[i] == helper[index]) {
-				res = -i - 1;
 			}
 		}
-		return res;
+		return true;
 	}
 
-	private static int checkOpenBrecket(char[] helper, int index) {
+	private static int checkBrecket(char[] helper, int index) {
 		int res = 0;
-		char[] breket = { '(', '[', '{' };
+		char[] breket = { '(', '[', '{', ')', ']', '}' };
 		for (int i = 0; i < breket.length; i++) {
-			if (breket[i] == helper[index]) {
+			if (breket[i] == helper[index] && i > 2) {
+				res = -i - 1 + 3;
+			}
+			if (breket[i] == helper[index] && i <= 2) {
 				res = i + 1;
 			}
 		}
-
 		return res;
 	}
-	
-	
 }
--- src/additionalTask/test/AdditionalTasksTest.java
+++ src/additionalTask/test/AdditionalTasksTest.java
@@ -9,26 +9,25 @@ class AdditionalTasksTest {
 
 	@Test
 	void hasValidBracketsTest() {
-		String str = "{[]}";
+		String str = "{qq[]}";
 		assertTrue(hasValidBrackets(str));
 		
-		String str1 = "{[]}(){[]}";
+		String str1 = "{[w]}(){[]}";
 		assertTrue(hasValidBrackets(str1));
 		
-		String str2 = "}{[]}(){[]}{";
+		String str2 = "q}{[]}(q){[]}{";
 		assertFalse(hasValidBrackets(str2));
 		
-		String str3 = "({)[]}(){[]}";
-		assertTrue(hasValidBrackets(str3));
+		String str3 = "qqq({)[]e}(){[]}eee";
+		assertFalse(hasValidBrackets(str3));
 		
-		String str4 = "{[]}{(}){[]}";
-		assertTrue(hasValidBrackets(str4));
+		String str4 = "{[]}{(q}){[]}";
+		assertFalse(hasValidBrackets(str4));
 	}
 	@Test
 	void isAnagramTest() {
 		String str1 = "lolo";
 		String str2 = "olol";
-		
 		String str3 = "dabalol";
 		String str4 = "looabda";
 		assertTrue(isAnagram(str1, str2));
